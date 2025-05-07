32a11138fdf9bc04a1ed4b896f34560ec0907d99
Roksana Akter <roksana18cse04@gmail.com>
2025-05-07 12:32:08 +0600

    image Gennerate Any Model

--- app/config.py
+++ app/config.py
@@ -2,7 +2,13 @@
 
 import os
 
+
+from pydantic import BaseModel
+
+class ImageRequest(BaseModel):
+    model_name: str
+    prompt: str
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "data", "data.json")
-
 
