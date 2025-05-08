4b4920fd183c16c11dbb8aaeffa48fcdcc46387e
marzia <marziasultanar01@gmail.com>
2025-05-08 17:11:34 +0600

    add multi agent

--- app/services/video_generation.py
+++ app/services/video_generation.py
@@ -1,4 +1,5 @@
 import requests
+from app.schemas.TextToVedio import TextToVideoRequest
 import time
 import os
 from dotenv import load_dotenv
@@ -46,9 +47,9 @@ def get_prediction(prediction_id):
         time.sleep(1)  # Wait before polling again
  
  
-def generate_video(prompt: str, model_name: str, duration: int) -> str:
+def generate_video(data: TextToVideoRequest) -> str:
     # Generate a video based on the prompt, model name, and duration
-    prediction_id = create_prediction(prompt, model_name, duration)
+    prediction_id = create_prediction(data.model_name, data.prompt, data.duration)
     try:
         video_url = get_prediction(prediction_id)
         url = video_url.get("output")
@@ -60,18 +61,18 @@ def generate_video(prompt: str, model_name: str, duration: int) -> str:
         print(f"Error while fetching prediction: {e}")
         raise
 
-# Example usage
-if __name__ == "__main__":
-    prompt = input("Enter the prompt for video generation: ")
-    model_name = input("Enter the model name: ")
-    duration = int(input("Enter the duration (in seconds): "))
+# # Example usage
+# if __name__ == "__main__":
+#     prompt = input("Enter the prompt for video generation: ")
+#     model_name = input("Enter the model name: ")
+#     duration = int(input("Enter the duration (in seconds): "))
 
-    try:
-        # Generate video and get the result
-        video_url = generate_video(prompt, model_name, duration)
-        if video_url:
-            print(f"Generated video URL: {video_url}")
-        else:
-            raise Exception("No video URL returned.")
-    except Exception as e:
-        print(f"Error: {e}")
+#     try:
+#         # Generate video and get the result
+#         video_url = generate_video(prompt, model_name, duration)
+#         if video_url:
+#             print(f"Generated video URL: {video_url}")
+#         else:
+#             raise Exception("No video URL returned.")
+#     except Exception as e:
+#         print(f"Error: {e}")
