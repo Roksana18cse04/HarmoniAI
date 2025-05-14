30b77703a2c28bdfe49c51a6dce0b17a33310e58
marzia <marziasultanar01@gmail.com>
2025-05-14 17:11:05 +0600

    add content creator agent

--- app/routes/content_generator_route.py
+++ app/routes/content_generator_route.py
@@ -1,2 +1,10 @@
 from fastapi import APIRouter
-from app.agents.content_agent import contentGenerate
+from app.agents.content_creator_agent import generate_content_from_instruction
+
+router= APIRouter()
+
+@router.post("/content-creator")
+async def content_creator_endpoint(instruction:str):
+    
+    response=generate_content_from_instruction(instruction)
+    return response
