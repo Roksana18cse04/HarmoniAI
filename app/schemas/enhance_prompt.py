c2a76a8b4afca9438209f929354e5d16d484b2aa
marzia <marziasultanar01@gmail.com>
2025-05-28 14:54:22 +0600

    add intend

--- app/schemas/enhance_prompt.py
+++ app/schemas/enhance_prompt.py
@@ -5,7 +5,7 @@ class EnhanceRequest(BaseModel):
     intend: str
 
 class EnhanceResponse(BaseModel):
-    status: str
     base_prompt: str
     target_model: str
     enhanced_prompt: str
+    intend: str
