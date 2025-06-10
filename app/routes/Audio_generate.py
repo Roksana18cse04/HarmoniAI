00f0b9a759da830f3930bca617a7b43158dfb2bd
Roksana Akter <roksana18cse04@gmail.com>
2025-06-10 11:07:13 +0600

    new route add

--- app/routes/Audio_generate.py
+++ app/routes/Audio_generate.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, UploadFile, File, Form
 from app.services.text_to_voice import text_to_audio_generate
-from app.schemas.text_to_audio import TextToAudioRequest
 
 router = APIRouter()
 
 @router.post("/audio-generate")
 async def audio_generate(prompt: str = Form(...)):
+
     result = text_to_audio_generate(prompt)
+    print(result)
     return {
         "status": "success",
         "prompt": prompt,
